7ddbd24f3d1feb432f0ad35d89a57098b1a21d23
abel armando <abelarmando79@gmail.com>
2025-03-21 10:39:28 +0700

    menambahkan searchbar dan membenarkan style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,39 +2,7 @@ import React from "react";
 import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
-import { Image, ImageBackground, Text, View } from "react-native";
-
-const TabIcon = ({
-  text,
-  imgbg,
-  imgicon,
-  focused,
-}: {
-  text: string;
-  imgbg: any;
-  imgicon: any;
-  focused: boolean;
-}) => {
-  if (focused) {
-    return (
-      <>
-        <ImageBackground
-          source={imgbg}
-          className="w-full rounded-full flex flex-row min-w-[100px] min-h-16 items-center justify-center gap-2 mt-4 overflow-hidden"
-        >
-          <Image source={imgicon} tintColor="#151312" className="size-5" />
-          <Text className="text-base font-semibold">{text}</Text>
-        </ImageBackground>
-      </>
-    );
-  }
-
-  return (
-    <View className="justify-end h-10">
-      <Image source={imgicon} className="size-5 " tintColor={"#A8B5DB"}></Image>
-    </View>
-  );
-};
+import TabIcon from "@/components/TabIcon";
 
 const _layout = () => {
   return (
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
-
+import { Image, ScrollView, Text, TextInput, View } from "react-native";
+import { images } from "@/constants/images";
+import React from "react";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
+// absolute translate-x-32 translate-y-40
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-5xl text-primary font-bold">test</Text>
+    <View className="bg-primary flex-1 ">
+      <Image source={images.bg} className="absolute w-full" />
+
+      <ScrollView
+        className="flex-1 w-full "
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+          alignItems: "center",
+          paddingTop: 80,
+          paddingHorizontal: 20,
+          gap: 10,
+        }}
+      >
+        <Image source={icons.logo} className="size-9" />
+        <SearchBar placeholder="Search for a movie" />
+      </ScrollView>
     </View>
   );
 }
