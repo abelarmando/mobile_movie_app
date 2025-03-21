f3c88f00e46f432a5d2357210ef3455fcc75d6c7
abel armando <abelarmando79@gmail.com>
2025-03-21 10:38:22 +0700

    membenearkan config untuk tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
@@ -9,6 +9,7 @@ module.exports = {
         accent: "#AB8BFF",
         bgcolor: "#0F0D23",
         detailcl: "#D6C7FF",
+        textclr: "#A8B5DB",
       },
     },
   },
